c9a7e5b0ddef91805bce3ca00e0d79d42afd4df3
Daniele Morosinotto <d.morosinotto@icloud.com>
2017-02-16 22:20:00 +0100

    manual serve even style.css + npm test

--- 2-staticfile/index.js
+++ 2-staticfile/index.js
@@ -2,8 +2,17 @@ var http = require("http");
 var path = require("path");
 var fs = require("fs");
 
+function static(file, res, ct = "text/html") {
+    res.writeHead(200,{"Content-Type": ct});
+    fs.createReadStream(path.join(__dirname,"assets",file))
+        .pipe(res);
+}
 
 http.createServer(function(req, res){
-    fs.createReadStream(path.join(__dirname,"assets","page.html"))
-        .pipe(res);
+    console.log(req.method, req.url);
+    if (req.url.indexOf("style.css")>0) {
+        static("style.css", res, "text/css");
+    } else {
+        static("page.html", res);
+    }
 }).listen(3002, () => console.log("Serving page.html on :3002"));

6c8ca26c4e464357c1a23a31efa1d7e5ea926335
Daniele Morosinotto <d.morosinotto@icloud.com>
2017-02-16 23:09:36 +0100

    add validation of params + return JSON my 1st RPC!

--- 3-querystring/index.js
+++ 3-querystring/index.js
@@ -2,9 +2,12 @@ var express = require("express");
 var app = express();
 
 app.get("/add", (req, res)=>{
-    var a = parseInt(req.query.a,10);
-    var b = parseInt(req.query.b,10);
-    res.end("ADD=" + (a+b));
+    if (isNaN(req.query.a) || isNaN(req.query.b)) {
+        res.status(400) //BAD REQUEST
+            .end("Invalid or missing querystring params a,b number!");
+    } else { 
+        res.send( { add: parseFloat(req.query.a) + parseFloat(req.query.b) } ) //RETURN JSON
+    }
 });
 
-app.listen(3003, ()=>"Express server running on :3003");
+app.listen(3003, ()=>console.log("Express server running on :3003"));

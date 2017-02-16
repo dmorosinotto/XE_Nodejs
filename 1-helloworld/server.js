09f181e08a6b903006fcdcff64eab37c57acdcdb
Daniele Morosinotto <d.morosinotto@icloud.com>
2017-02-16 21:45:57 +0100

    set statusCode and Header Content-Type

--- 1-helloworld/server.js
+++ 1-helloworld/server.js
@@ -1,5 +1,7 @@
 var http = require("http");
 
 http.createServer(function(req, res){
+    res.statusCode = 201;
+    res.setHeader("Content-Type", "text/plain");
     res.end("<h1>Hello world</h1>");
 }).listen(3001);

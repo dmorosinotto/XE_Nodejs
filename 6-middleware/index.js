95299adbf4426edf057aa6db70f92a1681e8c530
Daniele Morosinotto <d.morosinotto@icloud.com>
2017-02-17 00:47:50 +0100

    add callapi.html in 2-staticfile and duplicate 4-restapi If you try callapi it gives CORS error api is on different domain/port:3006 than page :3002

---
+++ 6-middleware/index.js
@@ -0,0 +1,29 @@
+var express = require("express");
+var app = express();
+var cors = require("cors");
+var users = require("xe-nodejs-mock")(1000);
+
+
+app.get("/api/users", function(req,res){
+    res.send(users); //JSON ARRAY
+});
+
+
+app.get("/api/users/:userid", (req,res)=> {
+    const id = parseInt(req.params.userid,10);
+    let idx = users.findIndex( u => u.id === id );
+    if (idx>=0) {
+        res.send(users[idx]); //JSON OBJECT
+    } else {
+        res.status(404).send(`Not found user with id=${id}`);
+    }
+});
+
+//ERROR HANDLER MIDDLEWARE
+app.use(function errorHandler(err, req, res, next) {
+    console.warn("Internal Server ERROR:", err.stack);
+    if (process.env.NODE_ENV === "production") delete err.stack;
+    res.status(500).send({ error: err });
+});
+
+app.listen(3006, ()=>console.log("Express with cors running on :3006/api/users"));

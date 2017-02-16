bb75a69b429f39e69941c21280a51f264f949d04
Daniele Morosinotto <d.morosinotto@icloud.com>
2017-02-16 21:55:59 +0100

    add PORT and HOST configuration via process.env

--- 1-helloworld/server.js
+++ 1-helloworld/server.js
@@ -1,7 +1,13 @@
 var http = require("http");
 
-http.createServer(function(req, res){
-    res.statusCode = 201;
+let server = http.createServer(function(req, res){
+    res.statusCode = 200;
     res.setHeader("Content-Type", "text/plain");
     res.end("<h1>Hello world</h1>");
-}).listen(3001);
+});
+
+const port = process.env.PORT || 3001;
+const host = process.env.HOST || "127.0.0.1";
+server.listen(port, () => {
+    console.log(`Server running at http://${host}:${port}...`);
+});

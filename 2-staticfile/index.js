f1a9e95285371cf4431b419e05a448abaa8a31ac
Daniele Morosinotto <d.morosinotto@icloud.com>
2017-02-16 22:47:05 +0100

    simplify code using express + static

--- 2-staticfile/index.js
+++ 2-staticfile/index.js
@@ -1,18 +1,7 @@
-var http = require("http");
+var express = require("express");
 var path = require("path");
-var fs = require("fs");
 
-function static(file, res, ct = "text/html") {
-    res.writeHead(200,{"Content-Type": ct});
-    fs.createReadStream(path.join(__dirname,"assets",file))
-        .pipe(res);
-}
-
-http.createServer(function(req, res){
-    console.log(req.method, req.url);
-    if (req.url.indexOf("style.css")>0) {
-        static("style.css", res, "text/css");
-    } else {
-        static("page.html", res);
-    }
-}).listen(3002, () => console.log("Serving page.html on :3002"));
+var app = express();
+app.use(express.static(path.join(__dirname,"assets")));
+app.get("/", (req,res) => res.sendFile(__dirname + "/assets/page.html"));
+app.listen(3002, () => console.log("Serving static with express on :3002"));
